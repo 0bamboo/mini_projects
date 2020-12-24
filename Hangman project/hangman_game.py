237a051045009c724f0dd91769752df81041f82f
Paayk <itsabdennacerama@gmail.com>
2020-12-24 16:30:24 +0100

    fix no problem

--- Hangman project/hangman_game.py
+++ Hangman project/hangman_game.py
@@ -1,6 +1,9 @@
 import random
 import time
 
+global pl_guess
+global the_word
+global hash_word
 print('\n \033[1;32;40m  Hi there, Welcome to the Hangman game \n')
 time.sleep(1.1337)
 name = input('\033[0;37;40m Please enter your name : ')
@@ -9,10 +12,8 @@ print("\n   Let's begin the game {} !  GOOD LUCK and HAVE FUN :) \n".format(name
 time.sleep(1.1337)
 print('   Remember to choose your letters wisely !! \n')
 time.sleep(1.1337)
-global pl_guess
-random_words = ['heelo', 'woorld', name, 'beeautifl', 'awsoeme', 'hnmgman', 'fuun', 'ggame']
+random_words = ['hello', 'world', name, 'beautiful', 'awesome', 'hangman', 'fun', 'game', 'congratulation']
 the_word = random.choice(random_words)
-global hash_word
 hash_word = '-'*len(the_word)
 
 def hang_the_player(count):
@@ -75,12 +76,17 @@ def hang_the_player(count):
               "   |       /|\ \n"
               "   |       / \ \n"
               "___|___        \n")
-        print('\033[1;31;40m **** GAME OVER ****')
+        print('\n\n\033[1;31;40m **** GAME OVER ****\n\n\n')
+
+def menu():
+    pass
 
 
-def thats_correct(the_word, pl_guess):
+def thats_correct(pl_guess):
     global hash_word
+    global the_word
     index = the_word.find(pl_guess)
+    the_word = the_word[:index] + '-' + the_word[index + 1:]
     hash_word = hash_word[:index] + pl_guess + hash_word[index + 1:]
     hash_remaining = hash_word.count('-')
     good = ['GOOD', 'NICE', 'PERFECT', 'AWESOME']
@@ -91,9 +97,10 @@ def thats_correct(the_word, pl_guess):
         print('\n\033[1;35;40m {} GUESS , {} more letters >[ {} ]< KEEP IT UP \n'.format(cngrts, hash_remaining,hash_word))
 
 
-def game_loop():
+def game_loop(random_words):
+    global the_word
+    global hash_word
     count = 1
-    play_again = ''
     while 1:
         pl_guess = input('\033[0;37;40m Enter your guess --> ')
         if len(pl_guess) > 1 or not pl_guess.isalpha():
@@ -103,20 +110,31 @@ def game_loop():
             hang_the_player(count)
             count += 1
         elif pl_guess in the_word:
-            thats_correct(the_word, pl_guess)
+            thats_correct(pl_guess)
             if '-' not in hash_word:
-                print('CONGRATULATION YOU WON !!!')
-                play_guess = input('if you want to play again press y [YES] if not press n [NO] : ')
+                print('\n\033[1;32;40m *********** CONGRATULATION YOU WON !!! ***********\n')
+                play_guess = input('\033[0;37;40m if you want to play again press y [YES] if not press n [NO] : ')
                 if play_guess.lower() == 'y':
-                    game_loop()
-                elif play_guess.lower() == 'n':
+                    print('\nSTARTING NEW GAME ....')
+                    time.sleep(1.1337)
+                    the_word = random.choice(random_words)
+                    hash_word = len(the_word) * '-'
+                    print('\n This Is The New Hangman Word : {}   As you see it contains {} letters, Please enter your guess |only one letter per time|'.format(hash_word, len(the_word)))
+                    game_loop(random_words)
+                elif play_guess.lower() == 'n' or play_guess:
                     break
+        #  SOMETHING IS WRONG HERE FIX IT !!!!!!!
         if count == 7:
-            play_guess = input('if you want to play again press y [YES] if not press n [NO] : ')
+            play_guess = input('\033[0;37;40m if you want to play again press y [YES] if not press n [NO] : ')
             if play_guess.lower() == 'y':
-                game_loop()
-            else:
+                print('\nSTARTING NEW GAME ....')
+                time.sleep(1.1337)
+                the_word = random.choice(random_words)
+                hash_word = len(the_word) * '-'
+                print('\n This Is The New Hangman Word : {}   As you see it contains {} letters, Please enter your guess |only one letter per time|'.format(hash_word, len(the_word)))
+                game_loop(random_words)
+            elif play_guess.lower() == 'n' or play_guess:
                 break
 
 print('\n This is the Hangman Word : {}   As you see it contains {} letters, Please enter your guess |only one letter per time|'.format(hash_word, len(the_word)))
-game_loop()
+game_loop(random_words)
