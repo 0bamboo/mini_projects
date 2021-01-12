7cf40d9afc96b688c893c311ee929e9bbfab1fa3
paayk <itsabdennacerama@gmail.com>
2021-01-12 23:03:58 +0100

    scraping

--- weatherscript/whatstheweather.py
+++ weatherscript/whatstheweather.py
@@ -1,3 +1,10 @@
-import pandas as pd 
+
 import requests
-from bs4 import beautifulsoup
+from bs4 import BeautifulSoup
+
+
+
+source = requests.get('http://www.google.com').text
+atr = BeautifulSoup(source, 'lxml')
+article = atr.find('div', id = 'guser')
+print(article.a.text)
