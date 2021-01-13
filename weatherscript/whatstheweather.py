a682647b018e0c56f33491320c0f17eae06d328a
paayk <itsabdennacerama@gmail.com>
2021-01-14 00:19:08 +0100

    done

--- weatherscript/whatstheweather.py
+++ weatherscript/whatstheweather.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 def _get_weather():
     page = requests.get("https://weather.com/weather/today/l/1078e192cff707be1411900bb06026d9eda1d6f65ad88eb396da69cf08fd1abe").text
     soup = BeautifulSoup(page, "html.parser")
-    # print(soup.find(class_ = "card Card--cardPadded--2jAM0 Card--card--4VS_Q CurrentConditions--card--1sxFO gradients--sunnyDay-contrast--3BFuJ CurrentConditions--gradient--2g3vw"))
     city = soup.find("div", class_ = 'CurrentConditions--header--3-4zi').h1.text
     city = city.split(',')[0]
     time = soup.find('div', class_ = "CurrentConditions--timestamp--1SWy5").text.split()[2] + ' ' + soup.find('div', class_ = "CurrentConditions--timestamp--1SWy5").text.split()[3]
