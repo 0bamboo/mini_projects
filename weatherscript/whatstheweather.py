95a4544bdaee229456f1e1abb3dc20fc007f7926
paayk <itsabdennacerama@gmail.com>
2021-01-14 00:42:39 +0100

    done

--- weatherscript/whatstheweather.py
+++ weatherscript/whatstheweather.py
@@ -5,11 +5,9 @@ from bs4 import BeautifulSoup
 def _get_weather():
     page = requests.get("https://weather.com/weather/today/l/1078e192cff707be1411900bb06026d9eda1d6f65ad88eb396da69cf08fd1abe").text
     soup = BeautifulSoup(page, "html.parser")
-    city = soup.find("div", class_ = 'CurrentConditions--header--3-4zi').h1.text
-    city = city.split(',')[0]
+    city = soup.find("div", class_ = 'CurrentConditions--header--3-4zi').h1.text.split(',')[0]
     time = soup.find('div', class_ = "CurrentConditions--timestamp--1SWy5").text.split()[2] + ' ' + soup.find('div', class_ = "CurrentConditions--timestamp--1SWy5").text.split()[3]
-    weather = int(soup.find('span', class_ = 'CurrentConditions--tempValue--3KcTQ').text[:-1])
-    weather = (weather - 32) * 5//9
+    weather = (int(soup.find('span', class_ = 'CurrentConditions--tempValue--3KcTQ').text[:-1]) - 32) * 5//9
     if weather < 13:
         feel = 'cold'
     elif weather >= 13 and weather <= 30:
