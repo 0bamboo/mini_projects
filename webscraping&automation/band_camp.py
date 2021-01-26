d7ac1e349e45a54c9a32c484b702349b55165202
Paayk <itsabdennacerama@gmail.com>
2021-01-26 15:42:41 +0100

    the second next

--- webscraping&automation/band_camp.py
+++ webscraping&automation/band_camp.py
@@ -4,9 +4,9 @@ from selenium.webdriver.common.by import By
 import time
 
 #the path for webdriver in my laptop:
-path = '/home/mugiwara/chromedriver.exe'
+# path = '/home/mugiwara/chromedriver.exe'
 #the path for chromedriver in imac:
-# path = ''
+path = '/Users/abdait-m/Cdriver/chromedriver'
 try:
     driver = webdriver.Chrome(path)
     driver.get('https://bandcamp.com')
@@ -14,28 +14,37 @@ try:
     play_button = driver.find_element_by_class_name('playbutton')
     play_button.click()
     tracks = driver.find_elements_by_class_name('discover-item')
-    for item in range(len(tracks)):
-        print(tracks[item])
-        tracks[item].click()
-        time.sleep(5)
+    print(len(tracks))
+    counter = 1
+    # for item in range(len(tracks)):
+    #     tracks[item].click()
+    #     time.sleep(2)
+    # tracks[3].click()
     for elem in driver.find_elements_by_class_name('item-page'):
         print(elem.text.lower().find('next'))
         if elem.text.lower().find('next') == 0:
             print(elem.text.lower().find('next'))
             next_button = elem
     next_button.click()
-    time.sleep(10)
+    time.sleep(5)
     tracks = driver.find_elements_by_class_name('discover-item')
-    for item in range(len(tracks)):
-        tracks[item].click()
-        time.sleep(5)
+    print(len(tracks))
+    tracks[9].click()
+    # assert len(tracks) == 8
+    # for item in range(8 * counter, len(tracks)):
+    #     tracks[item].click()
+    #     time.sleep(2)
     next_button = [elem for elem in driver.find_elements_by_class_name('item-page') if elem.text.lower().find('next') > -1]
-    next_button.click()
-    time.sleep(10)
+    next_button[0].click()
+    time.sleep(5)
     tracks = driver.find_elements_by_class_name('discover-item')
-    for item in range(len(tracks)):
-        tracks[item].click()
-        time.sleep(5)
+    counter += 1
+    print(len(tracks))
+    # for item in range(8 * counter, len(tracks)):
+    #     tracks[item].click()
+    #     time.sleep(2)
+    print(f'|{tracks[17]}|')
+    tracks[17].click()
     print(type(next_button))
 
 except:
