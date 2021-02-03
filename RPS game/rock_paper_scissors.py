719951f8eb932696a10ab99e3d54e51a7da60324
Paayk <itsabdennacerama@gmail.com>
2021-02-03 14:39:27 +0100

    rsp

--- RPS game/rock_paper_scissors.py
+++ RPS game/rock_paper_scissors.py
@@ -3,14 +3,36 @@ import random as  rd
 
 from tkinter import *
 
-
+# THE INITIALIZING OF THE WINDOW
 window = Tk()
-label = Label(text = "Hello from label", foreground = 'DarkTurquoise', background = 'LightSalmon', width = 50, height = 20)
-label.pack()
-new_button = Button(text = 'Heat me hahah')
-new_button.pack()
-entry = Entry(fg='yellow', bg = 'blue', width = '50')
-entry.pack()
-entry.get()
-print(entry.get())
+
+# A SIMPLE FRAME 
+frame_a = Frame()
+frame_b = Frame()
+
+# THIS HOW TO CREATE LABEL
+label_a = Label(master = frame_a, text = 'Im in frame_a')
+label_a.pack()
+
+label_b = Label(master = frame_b, text = 'Im in frame_b')
+label_b.pack()
+
+border_effects = {
+    'flat' : FLAT,
+    'sunken' : SUNKEN,
+    'raised' : RAISED,
+    'groove' : GROOVE,
+    'ridge' : RIDGE,
+}
+
+frame_b.pack()
+frame_a.pack()
+
+# THIS THE TYPES OF BORDER EFFECTS 
+for relief_name, relief in border_effects.items():
+    frame = Frame(master = window, relief = relief, borderwidth = 5)
+    frame.pack(side = LEFT)
+    label =  Label(master = frame, text = relief_name)
+    label.pack()
+
 window.mainloop()
