3700b6f7d6a6136a07c67ef2fcb1cc52fea9f975
Paayk <itsabdennacerama@gmail.com>
2021-02-08 15:43:37 +0100

    starting the rps project

--- RPS game/rps_game.py
+++ RPS game/rps_game.py
@@ -1,10 +1,28 @@
-from tkinter import *
+from Tkinter import *
 import random as rd
 
 
 window = Tk()
-window.rowconfigure(0, minsize = 500)
-window.columnconfigure(0, minsize = 500)
-label = Label(text = 'start your game')
-label.grid()
+window.title('ROCK PAPER SCISSOR')
+# window.geometry('400x200')
+
+window.rowconfigure(0, minsize = 200, weight = 1)
+window.columnconfigure(0, minsize = 400, weight = 1)
+
+frame_title = Frame(window)
+frame_game = Frame(window)
+
+label_1 = Label(frame_title, text = 'hello from frame title',)
+label_2 = Label(frame_game, text = 'hello from frame game', relief = RAISED, bg = 'yellow')
+label_3 = Label(frame_game, text = 'hello from frame game', )
+label_4 = Label(frame_game, text = 'hello from frame game', )
+
+label_1.pack(anchor = CENTER, padx = 3, pady = 3)
+label_2.grid(row = 0, column = 0, padx = 3, pady = 3)
+label_3.grid(row = 0, column = 1, padx = 3, pady = 3)
+label_4.grid(row = 0, column = 2, padx = 3, pady = 3)
+
+frame_title.grid(row = 0, column = 0, sticky = 'nsew')
+frame_game.grid(row = 1, column = 0, sticky = 'nsew')
+
 window.mainloop()
