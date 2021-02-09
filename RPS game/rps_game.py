25659c1d8fae90f5a2f7063b3ea3b49d170a8e90
Paayk <itsabdennacerama@gmail.com>
2021-02-09 11:49:10 +0100

    nothing added

--- RPS game/rps_game.py
+++ RPS game/rps_game.py
@@ -1,9 +1,9 @@
-from Tkinter import *
+from tkinter import *
 import random as rd
 
 
 window = Tk()
-window.title('ROCK PAPER SCISSOR')
+window.title('R x P x S')
 # window.geometry('400x200')
 
 window.rowconfigure(0, minsize = 200, weight = 1)
@@ -13,16 +13,16 @@ frame_title = Frame(window)
 frame_game = Frame(window)
 
 label_1 = Label(frame_title, text = 'hello from frame title',)
-label_2 = Label(frame_game, text = 'hello from frame game', relief = RAISED, bg = 'yellow')
-label_3 = Label(frame_game, text = 'hello from frame game', )
-label_4 = Label(frame_game, text = 'hello from frame game', )
+label_2 = Label(frame_game, text = 'hello from east', relief = RAISED, bg = 'yellow')
+label_3 = Label(frame_game, text = 'hello from center', )
+label_4 = Label(frame_game, text = 'hello from west', )
 
 label_1.pack(anchor = CENTER, padx = 3, pady = 3)
-label_2.grid(row = 0, column = 0, padx = 3, pady = 3)
-label_3.grid(row = 0, column = 1, padx = 3, pady = 3)
-label_4.grid(row = 0, column = 2, padx = 3, pady = 3)
+label_2.pack(anchor = 'se')
+label_3.pack(anchor = CENTER)
+label_4.pack(anchor = 'sw')
 
 frame_title.grid(row = 0, column = 0, sticky = 'nsew')
-frame_game.grid(row = 1, column = 0, sticky = 'nsew')
+frame_game.grid(row = 1, column = 0,)
 
 window.mainloop()
