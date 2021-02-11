fdd65c5c607c84e3753bd9162c99edb0bf5d5c12
Paayk <itsabdennacerama@gmail.com>
2021-02-11 10:37:49 +0100

    you need to pad the buttons not in grid method

--- RPS game/rps_game.py
+++ RPS game/rps_game.py
@@ -5,4 +5,7 @@ import random as rd
 window = Tk()
 window.title('R x P x S')
 
+example_entry = Entry(text = 'enter something', fg = 'yellow')
+example_entry.grid(row = 0, column = 0, rowspan = 3)
+
 window.mainloop()
