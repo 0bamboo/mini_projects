29013e7d14f2f553cace0ce4f6414e264900eb39
Paayk <itsabdennacerama@gmail.com>
2021-02-20 11:51:22 +0100

    calculator design

--- Ugly GUI miniP/calculator.py
+++ Ugly GUI miniP/calculator.py
@@ -1,92 +1,119 @@
 from tkinter import *
 
-window = Tk()
 
-# window.rowconfigure(0, minsize = 600)
-# window.columnconfigure(0, minsize = 600)
+class Calculator():
 
-# Define functions
+    def __init__(self):
 
-def insert_number(number):
-    """ This function for insert the number in the entry label """
+        self.window = Tk()
 
-    result_entry.insert(END, number)
+        # The label where to see the result of opertions
+        self.result_entry = Entry(width = 33, font = ('Courier', 18))
+        # The buttons of calculator
+        self.button_1 = Button(master = self.window, text = '1', command = lambda : self.insert_number(1))
+        self.button_2 = Button(master = self.window, text = '2', command = lambda : self.insert_number(2))
+        self.button_3 = Button(master = self.window, text = '3', command = lambda : self.insert_number(3))
+        self.button_minus = Button(master = self.window, fg = 'blue', text = '-', command = lambda : self.set_the_operation('-'))
 
-
-def clear():
-    """ This function for clearing entry label """
-
-    result_entry.delete(0, END)
-
-
-def set_the_operation(operation):
-    """ This functino for set which operation to use for calculating """
-
-    global result
-    global oper
-    entry_str = result_entry.get()
-    if entry_str:
-        result = int(result_entry.get())
-    oper = operation
-    result_entry.delete(0, END)
-
-
-def get_result():
-    """ This function for print the result of the operation in the entry label """
-
-    if result_entry.get() and oper:
-        second_number = int(result_entry.get())
-        result_entry.delete(0, END)
-        if oper == '+':
-            result_entry.insert(END, result + second_number)
-        elif oper == '*':
-            result_entry.insert(END, result * second_number)
-        elif oper == '-':
-            result_entry.insert(END, result - second_number)
-        elif oper == '/':
-            result_entry.insert(END, result / second_number)
+        self.button_4 = Button(master = self.window, text = '4', command = lambda : self.insert_number(4))
+        self.button_5 = Button(master = self.window, text = '5', command = lambda : self.insert_number(5))
+        self.button_6 = Button(master = self.window, text = '6', command = lambda : self.insert_number(6))
+        self.button_plus = Button(master = self.window, fg = 'blue', text = '+', command = lambda : self.set_the_operation('+'))
+        
+        self.button_7 = Button(master = self.window, text = '7', command = lambda : self.insert_number(7))
+        self.button_8 = Button(master = self.window, text = '8', command = lambda : self.insert_number(8))
+        self.button_9 = Button(master = self.window, text = '9', command = lambda : self.insert_number(9))
+        self.button_reset = Button(master = self.window, fg = 'blue', text = 'CE', command = self.clear)
+        
+        self.button_0 = Button(master = self.window, text = '0', command = lambda : self.insert_number(0))
+        self.button_multi = Button(master = self.window, fg = 'blue', text = '*', command = lambda : self.set_the_operation('*'))
+        self.button_div = Button(master = self.window, text = '/', command = lambda : self.set_the_operation('/'))
+        self.button_mod = Button(master = self.window, text = '%', command = lambda : self.set_the_operation('%'))
+        
+        self.button_equal = Button(master = self.window, fg = 'blue', text = '=', command = self.get_result)
 
 
-# The label where to see the result of opertions
-result_entry = Entry(width = 33, font = ('Comic sans serif', 15))
-result_entry.grid(columnspan = 4, padx = 10, pady = 10, ipadx = 15, ipady = 8)
+    def insert_number(self, number):
+        """ This function for insert the number in the entry label """
 
-# The buttons of calculator
-button_1 = Button(master = window, text = '1', padx = 30, pady = 10, command = lambda : insert_number(1))
-button_2 = Button(master = window, text = '2', padx = 30, pady = 10, command = lambda : insert_number(2))
-button_3 = Button(master = window, text = '3', padx = 30, pady = 10, command = lambda : insert_number(3))
-button_4 = Button(master = window, text = '4', padx = 30, pady = 10, command = lambda : insert_number(4))
-button_5 = Button(master = window, text = '5', padx = 30, pady = 10, command = lambda : insert_number(5))
-button_6 = Button(master = window, text = '6', padx = 30, pady = 10, command = lambda : insert_number(6))
-button_7 = Button(master = window, text = '7', padx = 30, pady = 10, command = lambda : insert_number(7))
-button_8 = Button(master = window, text = '8', padx = 30, pady = 10, command = lambda : insert_number(8))
-button_9 = Button(master = window, text = '9', padx = 30, pady = 10, command = lambda : insert_number(9))
-button_0 = Button(master = window, text = '0', padx = 30, pady = 10, command = lambda : insert_number(0))
-button_reset = Button(master = window, fg = 'blue', text = 'CE', padx = 25, pady = 10, command = clear)
-button_plus = Button(master = window, fg = 'blue', text = '+', padx = 30, pady = 10, command = lambda : set_the_operation('+'))
-button_minus = Button(master = window, fg = 'blue', text = '-', padx = 30, pady = 10, command = lambda : set_the_operation('-'))
-button_multi = Button(master = window, fg = 'blue', text = '*', padx = 30, pady = 10, command = lambda : set_the_operation('*'))
-button_equal = Button(master = window, fg = 'blue', text = '=', padx = 30, pady = 10, width = 12, command = get_result)
+        self.result_entry.insert(END, number)
 
-# The position of buttons
-button_7.grid(row = 1, column = 0, padx = 2)
-button_8.grid(row = 1, column = 1, padx = 2)
-button_9.grid(row = 1, column = 2, padx = 2)
-button_reset.grid(row = 1, column = 3)
 
-button_4.grid(row = 2, column = 0)
-button_5.grid(row = 2, column = 1)
-button_6.grid(row = 2, column = 2)
-button_plus.grid(row = 2, column = 3)
+    def clear(self):
+        """ This function for clearing entry label """
 
-button_1.grid(row = 3, column = 0)
-button_2.grid(row = 3, column = 1)
-button_3.grid(row = 3, column = 2)
-button_minus.grid(row = 3, column = 3)
+        self.result_entry.delete(0, END)
 
-button_equal.grid(row = 4, column = 0, columnspan = 2)
-button_multi.grid(row = 4, column = 3)
-button_0.grid(row = 4, column = 2)
 
-# To launch the program 
-window.mainloop()
+    def set_the_operation(self, operation):
+        """ This functino for set which operation to use for calculating """
+
+        global result
+        global oper
+        entry_str = self.result_entry.get()
+        if entry_str:
+            result = int(self.result_entry.get())
+        oper = operation
+        self.result_entry.delete(0, END)
+
+
+    def get_result(self):
+        """ This function for print the result of the operation in the entry label """
+
+        if self.result_entry.get() and oper:
+            second_number = int(self.result_entry.get())
+            self.result_entry.delete(0, END)
+            if oper == '+':
+                self.result_entry.insert(END, result + second_number)
+            elif oper == '*':
+                self.result_entry.insert(END, result * second_number)
+            elif oper == '-':
+                self.result_entry.insert(END, result - second_number)
+            elif oper == '/':
+                self.result_entry.insert(END, result / second_number)
+            elif oper == '%':
+                self.result_entry.insert(END, result % second_number)
+    
+
+    def build_widgets(self):
+        """ This function sets all the widget and all its data """
+
+        self.result_entry.grid(columnspan = 4, ipadx = 15, ipady = 15, sticky = E+N+W+S)
+        # The position of buttons
+        self.button_7.grid(row = 1, column = 0, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+        self.button_8.grid(row = 1, column = 1, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+        self.button_9.grid(row = 1, column = 2, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+        self.button_reset.grid(row = 1, column = 3, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+
+        self.button_4.grid(row = 2, column = 0, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+        self.button_5.grid(row = 2, column = 1, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+        self.button_6.grid(row = 2, column = 2, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+        self.button_plus.grid(row = 2, column = 3, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+
+        self.button_1.grid(row = 3, column = 0, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+        self.button_2.grid(row = 3, column = 1, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+        self.button_3.grid(row = 3, column = 2, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+        self.button_minus.grid(row = 3, column = 3, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+
+        self.button_0.grid(row = 4, column = 0, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+        self.button_mod.grid(row = 4, column = 1, ipadx = 10, ipady = 10, sticky = E+S+W+N)
+        self.button_div.grid(row = 4, column = 2, ipadx = 10, ipady = 10, sticky = E+S+N+W)
+        self.button_multi.grid(row = 4, column = 3, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+
+        
+        self.button_equal.grid(row = 5, column = 0, columnspan = 4, ipadx = 10, ipady = 10, sticky=E+W+N+S)
+        self.window.rowconfigure([0,1,2,3,4,5], weight = 1)
+        self.window.columnconfigure([0,1,2,3], weight = 1)
+
+
+    def launch_app(self):
+        """ For starting the calculator app """
+
+        self.build_widgets()
+        self.window.mainloop()
+
+
+
+if __name__ == "__main__":
+    start = Calculator()
+    start.launch_app()
